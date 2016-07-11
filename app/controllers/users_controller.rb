6424d58f2a53f160ceb9dfbc6c65e861b7732f52
RiniGarg <Rini.Garg@chronus.com>
2016-07-11 16:38:28 +0530

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -67,4 +67,8 @@ def index
     def admin_user
       redirect_to(root_url) unless current_user.admin?
     end
+    def show
+    @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
+  end
 end
